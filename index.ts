3a12970d9aa9efdcb95aef267eb1c7814fbef0e9
Josef Leventon <josef.leventon@gmail.com>
2023-03-27 13:18:34 -0400

    v0.3.3

--- index.ts
+++ index.ts
@@ -4,8 +4,8 @@ import SwiftProvider from './react/client/SwiftProvider'
 import useSwiftClient from './react/client/useSwiftClient'
 import useContract from './react/contract/useCommerce'
 
-import useTx from 'react/hooks/tx'
-import useToaster from 'react/hooks/useToaster'
+import useTx from './react/hooks/tx'
+import useToaster from './react/hooks/useToaster'
 
 // React components & contexts
 export { SwiftProvider, useSwiftClient, useContract, useTx, useToaster }
