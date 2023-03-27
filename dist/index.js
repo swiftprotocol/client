61f6b6a854eebe2f7572c0d7783742c3b48f5527
Josef Leventon <josef.leventon@gmail.com>
2023-03-27 13:18:42 -0400

    Publish ./ fix

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,8 @@ import { SwiftClient } from './core';
 import SwiftProvider from './react/client/SwiftProvider';
 import useSwiftClient from './react/client/useSwiftClient';
 import useContract from './react/contract/useCommerce';
-import useTx from 'react/hooks/tx';
-import useToaster from 'react/hooks/useToaster';
+import useTx from './react/hooks/tx';
+import useToaster from './react/hooks/useToaster';
 // React components & contexts
 export { SwiftProvider, useSwiftClient, useContract, useTx, useToaster };
 export { SwiftClient };
